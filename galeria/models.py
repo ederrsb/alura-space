2d2f62bea93a6e12abcd74255b27b4fe6bc28b41
ederrsb <ederrsb@gmail.com>
2024-04-04 18:30:42 -0300

    Incrementenado o Index

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 class Fotografia(models.Model):
@@ -14,6 +15,7 @@ class Fotografia(models.Model):
     descricao = models.TextField(null=False, blank=False)
     foto = models.CharField(max_length=100, null=False, blank=False)
     publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateField(default=datetime.now, blank=False)
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"    
