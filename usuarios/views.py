4a45dd555a21eeb7f6b069ec971c1b4af62425b0
ederrsb <ederrsb@gmail.com>
2024-04-15 13:23:47 -0300

    Alertas e Mensagens

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
@@ -20,8 +21,10 @@ def login(request):
             )
             if usuario is not None:
                 auth.login(request, usuario)
+                messages.success(request, f'{nome} logado com sucesso!')
                 return redirect('index')
             else:
+                messages.error(request, 'Erro ao efetuar login')
                 return redirect('login')    
 
     return render(request, 'usuarios/login.html', {'form': form})
@@ -34,6 +37,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form['senha_1'].value() != form['senha_2'].value():
+                messages.error(request, 'Senhas não são iguais!')
                 return redirect('cadastro')
             
             nome=form['nome_cadastro'].value()
@@ -41,6 +45,7 @@ def cadastro(request):
             senha=form['senha_1'].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existente')
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -49,6 +54,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, 'Cadastro efetuado com sucesso')
             return redirect('login')
 
     return render(request, 'usuarios/cadastro.html', {'form': form})
